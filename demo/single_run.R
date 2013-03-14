58448ceb257b7834471e7c4bf94c8218543dd53e
Joseph Guillaume <josephguillaume@gmail.com>
2013-03-14 12:27:57 +1100

    Working single run with only one pref curve allowed

--- demo/single_run.R
+++ demo/single_run.R
@@ -1,29 +1,69 @@
 library(indexCurveUncert)
-library(hydromad) ##eventseq
+##library(hydromad) ##eventseq
 loadIndexes("X:/phd/papers/namoi ua/Namoi model/Inputs EMS paper/index/")
 loadHydro("X:/phd/papers/namoi ua/Namoi model/Inputs EMS paper/",c("Pre90","Post90"))
 loadAssets("X:/phd/papers/namoi ua/Namoi model/Inputs EMS paper/ctf_v4.csv")
 
 specieslist <- c("RRGMS", "RRGRR", "BBMS", "BBRR", "LGMS", "LGRR", "WCMS", "WCRR")
 
+################################################################################
+
+## Extend  indices TODO: generalise
+##indexes[grep("dry",names(indexes))]
+## TODO: can't be Inf, need to be finite breakpoint, but needs to be big enough. Value determines slope, if any
+max.dry <- 25000
+for(s in c("RRGMS","BBMS","LGMS","WCMS","WCRR")){
+    cpt <- index.all[[sprintf("%s_%s.csv", s,"dry")]]
+    index.all[[sprintf("%s_%s.csv", s,"dry")]] <-
+        rbind(cpt,
+              c(Days=max.dry,rep(0,ncol(cpt)-1)))
+}
+for(s in c("RRGRR","BBRR","LGRR")){
+    cpt <- index.all[[sprintf("%s_%s.csv", s,"dry")]]
+    index.all[[sprintf("%s_%s.csv", s,"dry")]] <-
+        rbind(cpt,
+              c(Days=max.dry,rep(0.5,ncol(cpt)-1)))
+}
+
+
+##Duration
+for(s in c("RRGMS", "RRGRR", "BBMS", "BBRR", "LGMS", "LGRR"))
+    index.all[[sprintf("%s_%s.csv", s,"duration")]] <-
+        rbind(index.all[[sprintf("%s_%s.csv", s,"duration")]],
+              c(Days=1000,rep(NA,5)))
+
+checkAttributeRanges("Pre90",specieslist)
+checkAttributeRanges("Post90",specieslist)
+
+
+################################################################################
+
+
 st <- proc.time()
 xx <- envindex.diff.qp(scen="Pre90",baseline="Post90",
                        ecospecies=specieslist,
                        assetid=1,ctf=asset.table[1,7],
                        use.durs=c(T,F))
+proc.time()-st
 
+## Printing shows data.frame of results and settings
 print(xx)
+## Results can be subsetted, and return an envindex.bound object
 subset(xx,subset=species=="BBMS" & use.dur==TRUE)
 
-##win.metafile("bbms_duration_polygonconstraint_legend.emf",width=3.54,height=2.3)
+## Plot method shows preference curve for given attrib and subset
 par(mai=c(0.55,0.5,0.3,0.3),cex=0.55)
-## TODO: wrong sign
 plot(xx,attribs="duration",subset=species=="BBMS" & use.dur==FALSE,main=NA)
-legend("topright",c("Constraints","Favouring SYA","Favouring SYP"),fill=c(grey(0.9),NA,NA),col=c(NA,red,green),lty=c(NA,"dashed","dotted"),border=NA,lwd=c(NA,3,3))
-##dev.off()
+legend("topright",
+       c("Constraints","Favouring baseline","Favouring scenario"),
+       fill=c(grey(0.9),NA,NA),
+       col=c(NA,red(),green()),
+       lty=c(NA,"dashed","dotted"),
+       border=NA,lwd=c(NA,3,3))
 
+## Multiple plots can be made at once, for different species
 subset(xx,subset=use.dur==TRUE)
 
-par(mfrow=c(3,2))
+par(mfrow=c(4,2))
 plot(xx,attribs="duration",subset=use.dur==TRUE)
 
