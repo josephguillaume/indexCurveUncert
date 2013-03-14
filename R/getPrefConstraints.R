f85db334428670d35384e185e340c829219a6b76
Joseph Guillaume <josephguillaume@gmail.com>
2013-03-14 11:07:55 +1100

    getPrefConstraints works with single index

--- R/getPrefConstraints.R
+++ R/getPrefConstraints.R
@@ -2,13 +2,15 @@ getPrefConstraints <-
 function(species,attrib){
   cpt <- index.all[[sprintf("%s_%s.csv", species,attrib)]]
   ##Define absolute constraints
-  for(n in names(cpt)[-1]) cpt[,n] <- approxes.all[[sprintf("%s_%s.csv", species,attrib)]][[n]](cpt[,1])
-  bounds <- list(lower=apply(cpt[,-1,drop=F],1,min),upper=apply(cpt[,-1,drop=F],1,max))
+  for(n in names(cpt)[-1]) cpt[,n] <-
+    approxes.all[[sprintf("%s_%s.csv", species,attrib)]][[n]](cpt[,1])
+  bounds <- list(lower=apply(cpt[,-1,drop=F],1,min),
+                 upper=apply(cpt[,-1,drop=F],1,max))
 
   ## Define relative constraints, a-b>=0
   ss <- expand.grid(1:nrow(cpt),1:nrow(cpt))
   ss <- ss[ss[,1]!=ss[,2],]
-  constr <- do.call(rbind,apply(ss,1,function(x) cpt[x[1],-1]-cpt[x[2],-1]))
+  constr <- do.call(rbind,apply(ss,1,function(x) cpt[x[1],-1,drop=F]-cpt[x[2],-1,drop=F]))
   constr <- ifelse(constr>=0,">","<")
   constr <- data.frame( ##a=cpt[ss[,1],1],
                        ##b=cpt[ss[,2],1],
