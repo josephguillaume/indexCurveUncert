036a3a8d8248f8992680e6cc81078fcce59f7695
Joseph Guillaume <josephguillaume@gmail.com>
2013-04-05 15:54:58 +1100

    Allow multiple indices lists by merging their data.frames

--- R/load.R
+++ R/load.R
@@ -6,9 +6,28 @@
 ## ...
 ## approxes.all
 loadIndexes <- function(fname){
-  index.all <- lapply(dir(fname,
-                          pattern=".csv",full.names=T), read.csv, header=T)
-  names(index.all) <- dir(fname,pattern=".csv")
+  new.index.all <- lapply(dir(fname,
+                              pattern=".csv",full.names=T), read.csv, header=T)
+  names(new.index.all) <- dir(fname,pattern=".csv")
+  ## Merge indices
+  if(exists("index.all")){
+    print("Merging indices")
+    duplicate.names <- intersect(names(new.index.all),names(index.all))
+    if(length(duplicate.names)>0){
+      for(n in duplicate.names){
+        dup.cols <- intersect(names(index.all[[n]])[-1],names(new.index.all[[n]])[-1])
+        if(length(dup.cols)>0)
+          stop(sprintf("Columns of new %s index have same names as old. Not supported",n))
+        ## Merge new columns to old columns. If different number of bkpts, fills with NAs
+        index.all[[n]] <<- merge(index.all[[n]],new.index.all[[n]],all=T,by=1)
+        ## Remove from new - already added to old
+        new.index.all[[n]] <- NULL
+      }
+    }
+    ## Merge remaining - shouldn't be any duplicates
+    index.all <- modifyList(index.all,new.index.all)
+  } else { index.all <- new.index.all }
+  ## Used in plot and getPrefConstraints
   approxes.all <- lapply(index.all,
                          function(cpt) {
                            a <- lapply(2:ncol(cpt),function(i) approxfun(cpt[,1], cpt[,i], rule=2))
