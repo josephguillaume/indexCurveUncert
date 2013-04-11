8a26aad719e43076cf4491b4018a71285b84f93c
Joseph Guillaume <josephguillaume@gmail.com>
2013-04-11 17:11:23 +1000

    Fix for subset to use calling function's environment as enclos

--- R/subset.envindex.bound.R
+++ R/subset.envindex.bound.R
@@ -1,7 +1,8 @@
 subset.envindex.bound <- function(x,subset=T){
     e <- substitute(subset)
+    env <- parent.frame()
     out <- x[sapply(x,function(o) {
-        r <- eval(e,o,parent.frame())
+        r <- eval(e,o,env)
         r <- r & !is.na(r)
     })]
     class(out) <- class(x)
