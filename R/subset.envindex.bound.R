2ade3c3ad801d5df863354694cf0a6ee65c57fb5
Joseph Guillaume <josephguillaume@gmail.com>
2013-04-11 14:26:45 +1000

    Added as.data.frame.envindex.bound

--- R/subset.envindex.bound.R
+++ R/subset.envindex.bound.R
@@ -15,3 +15,10 @@ subset.envindex.bound <- function(x,subset=T){
     oldClass(y) <- cl
     y
 }
+
+
+as.data.frame.envindex.bound <- function(x,row.names=NULL,optional=FALSE,...){
+  keep.cols <- which(sapply(x[[1]],length)==1)
+  ##keep.cols <- c("assetid","ctf","species","diff.min","diff.max","use.dur")
+  do.call(rbind,lapply(x,function(r) as.data.frame(r[keep.cols])))
+}
