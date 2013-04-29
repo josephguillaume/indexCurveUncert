19ddd2d791a2664dee734004b96f912a3bbe4c67
Joseph Guillaume <josephguillaume@gmail.com>
2013-04-29 18:21:19 +1000

    Marginalise allows NAs to be ignored, allowing ambiguity

--- R/marginalise.R
+++ R/marginalise.R
@@ -1,15 +1,18 @@
-marginalise <- function(tabm,form,verbose=F){
+marginalise <- function(tabm,form,verbose=F,na.rm=F){
   if (verbose) {
     mar.vars <- setdiff(names(tabm),c(all.vars(form),"value"))
     cat("Retaining variables: ",paste(setdiff(all.vars(form),c("value",".")),collapse=","),"\n")
     cat("Marginalising over variables: ",paste(mar.vars,collapse=","),"\n")
+    if(na.rm) cat("Ignoring NAs\n")
   }
   answers <- cast(tabm,
                   formula=form,
                   fun.aggregate=function(s) {
                     s <- unique(as.character(s))
-                    if(length(s)==1) return(s)
-                    else return("Uncertain")
+                    if(na.rm) s <- na.omit(s)
+                    if(length(s)==1) {return(s)
+                    } else if(length(s)==0) { return(NA)
+                    } else return("Uncertain")
                   }
                   )
   answers
